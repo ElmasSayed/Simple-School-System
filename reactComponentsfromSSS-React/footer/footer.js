66fa42c4d5c43c3c316eb81deee21b582e1f8028
Elmas Sayed <elmassayed@gmail.com>
2018-07-25 23:33:18 -0700

    footer

---
+++ reactComponentsfromSSS-React/footer/footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './footer.css';
+
+class Footer extends Component {
+    render() {
+        return (
+            // <div className="Footer">
+                <footer className="container-fluid footer text-center">
+                    <p>SSS made by <a href="http://www.elmassayed.com" >www.elmassayed.com</a></p> 
+                </footer>
+            // </div>    
+        );
+    }
+}
+
+export default Footer;
